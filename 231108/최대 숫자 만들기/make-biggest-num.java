4f100b2ac6fbbf0eb3236949c394bf65e85e869e
최선빈 <105468810+hurontrara@users.noreply.github.com>
2023-11-08 17:07:56 +0900

    Title: 최대 숫자 만들기, Time: 1545ms, Memory: 88MB, Status: Passed - CodeTree

--- 231108/최대 숫자 만들기/make-biggest-num.java
+++ 231108/최대 숫자 만들기/make-biggest-num.java
@@ -6,21 +6,27 @@ public class Main {
     static class Point implements Comparable<Point> {
 
         int value;
+        int number;
 
-        Point(int value) {
+        Point(int value, int number) {
             this.value = value;
+            this.number = number;
         }
 
         public int compareTo(Point point) {
 
-            String formerString = String.valueOf(this.value);
-            String latterString = String.valueOf(point.value);
+            if (this.value == point.value) {
+                return this.number - point.number;
+            }
+
+            String formerString = Integer.toString(this.value);
+            String latterString = Integer.toString(point.value);
 
             String formerLatterString = formerString + latterString;
             String latterFormerString = latterString + formerString;
 
 
-            return Long.compare(Long.parseLong(latterFormerString), Long.parseLong(formerLatterString));
+            return latterFormerString.compareTo(formerLatterString);
         }
 
 
@@ -37,14 +43,12 @@ public class Main {
     public static void main(String[] args) {
 
         for (int i = 0; i < n; i++) {
-            treeSet.add(new Point(sc.nextInt()));
+            treeSet.add(new Point(sc.nextInt(), i));
         }
 
-        while (!treeSet.isEmpty()) {
-            System.out.print(treeSet.first().value);
-            treeSet.remove(treeSet.first());
+        for (Point point : treeSet) {
+            System.out.print(point.value);
         }
-
 
     }
 
