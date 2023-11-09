024c5dfaa165c66243142ac9eb5cf2b41b83d677
최선빈 <105468810+hurontrara@users.noreply.github.com>
2023-11-09 13:28:07 +0900

    Title: 소괄호 매칭 수 최대화하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231109/소괄호 매칭 수 최대화하기/maximize-the-number-of-parenthesis-matches.java
@@ -0,0 +1,88 @@
+import java.lang.annotation.Target;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    static class Point implements Comparable<Point> {
+
+        String value;
+        int typeOneCount;
+        int typeTwoCount;
+
+        Point(String value, int typeOneCount, int typeTwoCount) {
+            this.value = value;
+            this.typeOneCount = typeOneCount;
+            this.typeTwoCount = typeTwoCount;
+        }
+
+        public int compareTo(Point point) {
+
+            long leftScore = (long) this.typeOneCount * point.typeTwoCount;
+            long rightScore = (long) this.typeTwoCount * point.typeOneCount;
+
+            if (leftScore >= rightScore)
+                return -1;
+
+            return 1;
+
+        }
+
+
+    }
+
+    static Scanner sc = new Scanner(System.in);
+    static int n = sc.nextInt();
+
+    static Point[] pointArray = new Point[n];
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < n; i++) {
+            String input = sc.next();
+
+            String[] split = input.split("");
+
+            int leftCount = 0;
+            int rightCount = 0;
+            for (String s : split) {
+
+                if (s.equals("("))
+                    leftCount++;
+                else
+                    rightCount++;
+
+            }
+
+            pointArray[i] = new Point(input, leftCount, rightCount);
+
+
+        }
+
+        Arrays.sort(pointArray);
+
+        int leftCount = 0;
+        long totalNum = 0;
+        for (Point point : pointArray) {
+
+            String[] split = point.value.split("");
+
+            for (String s : split) {
+
+                if (s.equals("("))
+                    leftCount++;
+                else
+                    totalNum += leftCount;
+
+            }
+
+        }
+
+        System.out.println(totalNum);
+
+
+
+    }
+
+
+}
