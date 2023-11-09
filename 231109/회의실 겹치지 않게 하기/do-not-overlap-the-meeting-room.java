8b565bb50849bbee963995e45b67fbbe54c4839e
최선빈 <105468810+hurontrara@users.noreply.github.com>
2023-11-09 15:40:01 +0900

    Title: 회의실 겹치지 않게 하기, Time: 582ms, Memory: 23MB, Status: Passed - CodeTree

--- 231109/회의실 겹치지 않게 하기/do-not-overlap-the-meeting-room.java
+++ 231109/회의실 겹치지 않게 하기/do-not-overlap-the-meeting-room.java
@@ -1,107 +1,51 @@
-import java.util.PriorityQueue;
-import java.util.Scanner;
-import java.util.TreeSet;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static class Pair implements Comparable<Pair> {
+	static class Meeting implements Comparable<Meeting>{
+		int start;
+		int end;
+		
+		public Meeting(int start, int end) {
+			this.start =start;
+			this.end = end;
+		}
 
-        int start;
-        int end;
-
-        Pair(int start, int end) {
-            this.start = start;
-            this.end = end;
-        }
-
-
-        @Override
-        public int compareTo(Pair pair) {
-
-            if (this.start == pair.start)
-                return this.end - pair.end;
-
-            return this.start - pair.start;
-
-        }
-    }
-
-
-    static class Point implements Comparable<Point> {
-
-        int start;
-        int end;
-        int distance;
-
-        Point(int start, int end) {
-            this.start = start;
-            this.end = end;
-            this.distance = end - start;
-        }
-
-
-        @Override
-        public int compareTo(Point point) {
-
-            if (this.start == point.start && this.end == point.end)
-                return 0;
-
-            if (this.distance == point.distance)
-                return this.start - point.start;
-
-            return this.distance - point.distance;
-
-        }
-
-
-
-    }
-
-    static Scanner sc = new Scanner(System.in);
-    static int n = sc.nextInt();
-
-    static PriorityQueue<Point> queue = new PriorityQueue<>();
-
-
-    public static void main(String[] args) {
-
-        for (int i = 0; i < n; i++) {
-            queue.add(new Point(sc.nextInt(), sc.nextInt()));
-        }
-
-        greedy();
-
-    }
-
-    private static void greedy() {
-
-        TreeSet<Pair> pairSet = new TreeSet<>();
-
-
-        int cnt = 0;
-        while (!queue.isEmpty()) {
-
-            Point poll = queue.poll();
-
-            Pair leftPair = pairSet.floor(new Pair(poll.end, Integer.MAX_VALUE));
-            Pair rightPair = pairSet.ceiling(new Pair(poll.start, Integer.MIN_VALUE));
-
-            if (leftPair != null && leftPair.end > poll.start)
-                continue;
-
-            if (rightPair != null && rightPair.start < poll.end)
-                continue;
-
-            cnt++;
-            pairSet.add(new Pair(poll.start, poll.end));
-
-
-        }
-
-        System.out.println(n - cnt);
-
-
-    }
-
-
+		@Override
+		public int compareTo(Meeting o) {
+			if(this.end>o.end) return 1;
+			else if(this.end<o.end) return -1;
+			else return this.start-o.start;
+		}
+	}
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		List<Meeting> list = new ArrayList<>();
+		StringTokenizer st = null;
+		for(int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			list.add(new Meeting(a,b));
+		}
+		
+		Collections.sort(list);
+		
+		int endTime=0, cnt=0;
+		for(Meeting m : list) {
+			if(endTime<=m.start) {
+				endTime = m.end;
+				cnt++;
+			}
+		}
+		System.out.println(n - cnt);
+	}
 }
