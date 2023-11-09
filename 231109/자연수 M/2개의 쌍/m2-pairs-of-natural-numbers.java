92e147fef9b4c9cbee8eb6641b3a8e6b95937610
최선빈 <105468810+hurontrara@users.noreply.github.com>
2023-11-09 11:16:45 +0900

    Title: 자연수 M/2개의 쌍, Time: 1278ms, Memory: 54MB, Status: Passed - CodeTree

--- 231109/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.java
+++ 231109/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.java
@@ -1,22 +1,42 @@
-import java.util.Map;
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.TreeMap;
 
 public class Main {
+
+    static class Point implements Comparable<Point> {
+
+        int count;
+        int value;
+
+        Point(int count, int value) {
+            this.count = count;
+            this.value = value;
+        }
+
+
+        @Override
+        public int compareTo(Point point) {
+            if (this.value == point.value)
+                return this.count - point.count;
+
+            return this.value - point.value;
+        }
+
+    }
 
     static Scanner sc = new Scanner(System.in);
     static int n = sc.nextInt();
 
-    static TreeMap<Integer, Integer> treeMap = new TreeMap<>();
+    static Point[] array = new Point[n];
+
 
     public static void main(String[] args) {
 
         for (int i = 0; i < n; i++) {
-            int count = sc.nextInt();
-            int num = sc.nextInt();
-
-            treeMap.put(num, count);
+            array[i] = new Point(sc.nextInt(), sc.nextInt());
         }
+
+        Arrays.sort(array);
 
         greedy();
 
@@ -25,23 +45,34 @@ public class Main {
 
     private static void greedy() {
 
+        int left = 0;
+        int right = n - 1;
+
         int maxValue = 0;
-        while (treeMap.size() != 0) {
+        while (left <= right) {
 
-            Map.Entry<Integer, Integer> firstEntry = treeMap.firstEntry();
-            Map.Entry<Integer, Integer> lastEntry = treeMap.lastEntry();
+            Point leftPoint = array[left];
+            Point rightPoint = array[right];
 
-            maxValue = Math.max(maxValue, firstEntry.getKey() + lastEntry.getKey());
+            maxValue = Math.max(maxValue, leftPoint.value + rightPoint.value);
 
-            if (firstEntry.getValue() == 1)
-                treeMap.remove(firstEntry.getKey());
-            else
-                treeMap.put(firstEntry.getKey(), firstEntry.getValue() - 1);
+            int leftCount = leftPoint.count;
+            int rightCount = rightPoint.count;
 
-            if (lastEntry.getValue() == 1)
-                treeMap.remove(lastEntry.getKey());
-            else
-                treeMap.put(lastEntry.getKey(), lastEntry.getValue() - 1);
+            if (leftCount < rightCount) {
+                left++;
+                rightPoint.count -= leftCount;
+                continue;
+            }
+
+            if (leftCount > rightCount) {
+                right--;
+                leftPoint.count -= rightCount;
+                continue;
+            }
+
+            left++;
+            right--;
 
         }
 
@@ -49,5 +80,6 @@ public class Main {
 
 
     }
+
 
 }
