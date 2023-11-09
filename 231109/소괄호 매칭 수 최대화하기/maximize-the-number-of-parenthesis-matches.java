61abccda3ae8e32a81409fb7ca8542b2d57bf28c
최선빈 <105468810+hurontrara@users.noreply.github.com>
2023-11-09 13:39:38 +0900

    Title: 소괄호 매칭 수 최대화하기, Time: 597ms, Memory: 36MB, Status: Passed - CodeTree

--- 231109/소괄호 매칭 수 최대화하기/maximize-the-number-of-parenthesis-matches.java
+++ 231109/소괄호 매칭 수 최대화하기/maximize-the-number-of-parenthesis-matches.java
@@ -1,90 +1,70 @@
-import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Arrays;
+
+class Element implements Comparable<Element> {
+    int open, closed;
+
+    public Element(int open, int closed) {
+        this.open = open;
+        this.closed = closed;
+    }
+
+    @Override
+    public int compareTo(Element e) {
+        // (this, e), (e, this) 중
+        // 점수를 더 많이 얻을 수 있는 경우로 정렬합니다.
+        return e.open * this.closed - this.open * e.closed; 
+    }
+}
 
 public class Main {
-
-    static class Point implements Comparable<Point> {
-
-        String value;
-        int typeOneCount;
-        int typeTwoCount;
-
-        Point(String value, int typeOneCount, int typeTwoCount) {
-            this.value = value;
-            this.typeOneCount = typeOneCount;
-            this.typeTwoCount = typeTwoCount;
-        }
-
-        public int compareTo(Point point) {
-
-            long leftScore = (long) this.typeOneCount * point.typeTwoCount;
-            long rightScore = (long) this.typeTwoCount * point.typeOneCount;
-
-            if (leftScore > rightScore)
-                return -1;
-
-            if (leftScore == rightScore)
-                return 0;
-
-            return 1;
-
-        }
-
-
-    }
-
-    static Scanner sc = new Scanner(System.in);
-    static int n = sc.nextInt();
-
-    static Point[] pointArray = new Point[n];
+    public static final int MAX_N = 100000;
+    
+    // 변수 선언
+    public static int n;
+    // (개수, )개수를 쌍으로 하여 관리합니다.
+    public static Element[] brackets = new Element[MAX_N];
+    
+    public static long ans;
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
+        for(int i = 0; i < n; i++) {
+            String s = sc.next();
+            int open = 0, closed = 0; // 열린 소괄호, 닫힌 소괄호 수를 셉니다.
+            for(int j = 0; j < s.length(); j++) {
+                if(s.charAt(j) == '(')
+                    open++;
+                else {
+                    closed++;
 
-        for (int i = 0; i < n; i++) {
-            String input = sc.next();
-
-            String[] split = input.split("");
-
-            int leftCount = 0;
-            int rightCount = 0;
-            for (String s : split) {
-
-                if (s.equals("("))
-                    leftCount++;
-                else
-                    rightCount++;
-
+                    // 해당 문자열 만으로 얻게 되는 점수는
+                    // 미리 답에 더해줍니다.
+                    ans += open;
+                }
             }
-
-            pointArray[i] = new Point(input, leftCount, rightCount);
-
-
+            brackets[i] = new Element(open, closed);
         }
 
-        Arrays.sort(pointArray);
+        Arrays.sort(brackets, 0, n);
 
-        int leftCount = 0;
-        long totalNum = 0;
-        for (Point point : pointArray) {
+        // 정렬된 순서대로 보며
+        // 각 문자열을 붙였을 때
+        // 새롭게 얻게되는 점수를 더해줍니다.
+        int openSum = 0;
+        for(int i = 0; i < n; i++) {
+            int open = brackets[i].open;
+            int closed = brackets[i].closed;
 
-            String[] split = point.value.split("");
+            // 답을 갱신해줍니다.
+            ans += (long) openSum * closed;
 
-            for (String s : split) {
-
-                if (s.equals("("))
-                    leftCount++;
-                else
-                    totalNum += leftCount;
-
-            }
-
+            // openSum을 누적해줍니다.
+            openSum += open;
         }
 
-        System.out.println(totalNum);
-
-
-
+        System.out.print(ans);
     }
-
-
 }
