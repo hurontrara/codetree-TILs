c3ff735b0ad63eb5815d279614135e6f5e82b476
최선빈 <105468810+hurontrara@users.noreply.github.com>
2023-11-10 08:13:17 +0900

    Title: G & H 반전시키기, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 231109/G & H 반전시키기/reversing-g-and-h.java
+++ 231109/G & H 반전시키기/reversing-g-and-h.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
@@ -12,40 +11,31 @@ public class Main {
     static String nString = sc.next();
     static String mString = sc.next();
     
-    static String[] nStringArray = nString.split("");
-    static String[] mStringArray = mString.split("");
 
-    static List<Integer> changeList = new ArrayList<>();
+    static List<Integer> changeList = new LinkedList<>();
     
     public static void main(String[] args) {
 
-        for (int i = 0; i < n; i++) {
+        int ans = 0;
+        boolean mismatched = false;
 
-            String nChar = nStringArray[i];
-            String mChar = mStringArray[i];
+        for(int i = 0; i < n; i++) {
+            if(nString.charAt(i) != mString.charAt(i)) {
+                // 다시 미스매치가 처음으로 발생하는 순간이라면
+                // 답을 갱신해줍니다.
+                if(!mismatched) {
+                    mismatched = true;
+                    ans++;
+                }
+            }
+            else
+                mismatched = false;
+        }
+        System.out.print(ans);
 
-            if (!nChar.equals(mChar))
-                changeList.add(i);
 
         }
 
-        for (int i = 0; i < changeList.size() - 1; i++) {
-
-            Integer leftValue = changeList.get(i);
-            Integer rightValue = changeList.get(i + 1);
-
-            if (rightValue.equals(leftValue + 1))
-                changeList.remove(leftValue);
-
-
-        }
-
-        System.out.println(changeList.size());
 
 
     }
-
-
-    
-    
-}
