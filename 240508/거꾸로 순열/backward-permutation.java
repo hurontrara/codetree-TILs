23cdad82c7a714f90732cb276459459fa018cf4e
최선빈 <105468810+hurontrara@users.noreply.github.com>
2024-05-08 16:32:38 +0900

    Title: 거꾸로 순열, Time: 1187ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240508/거꾸로 순열/backward-permutation.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n;
+    static List<Integer> list = new ArrayList<>();
+    static int[] visited;
+
+    public static void main(String[] args) throws Exception {
+
+        // 초기화
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        visited = new int[n];
+
+        // 로직
+        recursive(0);
+
+
+    }
+
+    static void recursive(int index) {
+
+
+        // 출력
+        if (index == n) {
+
+            for (int i = 0; i < n; i++) {
+                System.out.print(list.get(i) + " ");
+            }
+            System.out.println();
+
+            return;
+
+        }
+
+        for (int i = n; i > 0; i--) {
+
+            if (visited[i - 1] == 0) {
+
+                visited[i - 1] = 1;
+                list.add(i);
+
+                recursive(index + 1);
+
+                visited[i - 1] = 0;
+                list.remove(list.size() - 1);
+
+            }
+
+
+        }
+ 
+
+    }
+
+
+
+
+}
