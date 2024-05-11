96203969e76a9470cab61f53eb6d89eea34f003d
최선빈 <105468810+hurontrara@users.noreply.github.com>
2024-05-11 20:11:23 +0900

    Title: 정수 사각형 최소 합, Time: 337ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240511/정수 사각형 최소 합/minimum-sum-path-in-square.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Stream;
+
+
+public class Main {
+
+    static int size;
+    static int[][] matrix;
+    static int[][] dpMatrix;
+
+    public static void main(String[] args) throws Exception {
+        // init
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        size = Integer.parseInt(br.readLine()); matrix = new int[size][size]; dpMatrix = new int[size][size];
+
+        for (int i = 0; i < size; i++) {
+            matrix[i] = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        }
+
+        dpMatrix[0][size - 1] = matrix[0][size - 1];
+        for (int i = size - 2; i >= 0; i--) {
+            dpMatrix[0][i] = dpMatrix[0][i + 1] + matrix[0][i];
+        }
+        for (int i = 1; i < size; i++) {
+            dpMatrix[i][size - 1] = dpMatrix[i - 1][size - 1] + matrix[i][size - 1];
+        }
+
+        // logic
+        System.out.print(memo(size - 1, 0));
+
+
+
+    }
+
+    static int memo(int row, int col) {
+
+        if (dpMatrix[row][col] != 0)
+            return dpMatrix[row][col];
+
+        
+        dpMatrix[row][col] = Math.min(memo(row - 1, col), memo(row, col + 1)) + matrix[row][col];
+
+        return dpMatrix[row][col];
+
+    }
+
+
+
+}
