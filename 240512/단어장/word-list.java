e3e2cbed85bcf761ac84ab3787b9d0451f7970fb
최선빈 <105468810+hurontrara@users.noreply.github.com>
2024-05-12 20:56:39 +0900

    Title: 단어장, Time: 658ms, Memory: 23MB, Status: Passed - CodeTree

---
+++ 240512/단어장/word-list.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static int n;
+
+    static final TreeMap<String, Integer> treeMap = new TreeMap<>();
+
+    public static void main(String[] args) throws Exception {
+
+        n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+
+            String input = br.readLine();
+
+            treeMap.put(input, treeMap.getOrDefault(input, 0) + 1);
+
+
+        }
+
+        for (Map.Entry<String, Integer> entry : treeMap.entrySet()) {
+
+            System.out.println(entry.getKey() + " " + entry.getValue());
+
+
+        }
+
+
+    }
+
+
+}
