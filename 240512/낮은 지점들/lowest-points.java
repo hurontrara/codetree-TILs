6e4b96b2c4cb85bccbc1114cb8513a09a616eb84
최선빈 <105468810+hurontrara@users.noreply.github.com>
2024-05-12 18:59:04 +0900

    Title: 낮은 지점들, Time: 749ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240512/낮은 지점들/lowest-points.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+
+
+public class Main {
+
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int size;
+    static final HashMap<Integer, Integer> hashMap = new HashMap<>();
+    static int answer = 0;
+
+    public static void main(String[] args) throws Exception {
+        
+        size = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < size; i++) {
+
+            int[] array = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+            int x = array[0]; int y = array[1];
+
+            if (hashMap.containsKey(x)) {
+
+                int currentValue = hashMap.get(x);
+                hashMap.put(x, Math.min(y, currentValue));
+
+            } else {
+
+                hashMap.put(x, y);
+
+            }
+
+        }
+
+
+        for (int y : hashMap.values()) {
+
+            answer += y;
+
+        }
+        System.out.print(answer);
+
+
+    }
+
+}
