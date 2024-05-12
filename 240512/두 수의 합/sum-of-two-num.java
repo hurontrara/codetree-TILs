13f2b2a39e8641ee7f5fd9aa9eb558eb75aa8217
최선빈 <105468810+hurontrara@users.noreply.github.com>
2024-05-12 12:51:08 +0900

    Title: 두 수의 합, Time: 247ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240512/두 수의 합/sum-of-two-num.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Stream;
+
+
+public class Main {
+
+    static int size, k;
+    static int[] array;
+
+    static int answer = 0;
+
+    static HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+    public static void main(String[] args) throws Exception {
+
+        // init
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] skArray = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray(); size = skArray[0]; k = skArray[1];
+
+        array = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        for (int value : array) {
+
+            hashMap.put(value, hashMap.getOrDefault(value, 0) + 1);
+
+        }
+
+        // logic
+        for (int key : hashMap.keySet()) {
+
+            int counterKey = k - key;
+
+            int value = hashMap.get(key);
+            int counterValue = hashMap.getOrDefault(counterKey, 0);
+
+            answer += (value * counterValue);
+
+        }
+
+        //print
+        System.out.print(answer / 2);
+
+
+
+    }
+
+
+
+
+}
