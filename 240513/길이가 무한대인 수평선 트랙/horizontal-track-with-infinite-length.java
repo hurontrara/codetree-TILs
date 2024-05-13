7b5b44b294790bf349f8331cebe43d32eff71d6f
최선빈 <105468810+hurontrara@users.noreply.github.com>
2024-05-13 22:40:43 +0900

    Title: 길이가 무한대인 수평선 트랙, Time: 1312ms, Memory: 59MB, Status: Passed - CodeTree

--- 240513/길이가 무한대인 수평선 트랙/horizontal-track-with-infinite-length.java
+++ 240513/길이가 무한대인 수평선 트랙/horizontal-track-with-infinite-length.java
@@ -1,79 +1,79 @@
-import java.io.*;
-import java.util.*;
-
-class Person implements Comparable<Person> {
-
-    long x;
-    long y;
-    long distance;
-
-    Person(long x, long y) {
-
-        this.x = x;
-        this.y = y;
-        this.distance = y - x;
-
-    }
-
-    @Override
-    public int compareTo(Person person) {
-
-        return person.distance - this.distance >= 0 ? 1 : -1;
-
-    }
-
-
-}
-
+import java.util.Scanner;
+import java.util.TreeSet;
 
 public class Main {
 
-    static int n, t; // n명의 사람, t초의 시간
+    static class Human implements Comparable<Human> {
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
+        int firstPosition;
+        int speed;
+        long lastPosition;
 
-    static TreeSet<Person> treeSet = new TreeSet<>();
-    static TreeSet<Long> memoSet = new TreeSet<>();
-    
-    public static void main(String[] args) throws Exception {
-        
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken()); t = Integer.parseInt(st.nextToken());
+        Human(int firstPosition, int speed) {
+            this.firstPosition = firstPosition;
+            this.speed = speed;
+            this.lastPosition = firstPosition + (long) speed * t;
+        }
 
-        for (int i = 0; i < n; i++) {
+        public int compareTo(Human human) {
 
-            st = new StringTokenizer(br.readLine());
-            long x = Long.parseLong(st.nextToken()); long speed = Long.parseLong(st.nextToken()); long y = x + speed * t;
-
-            treeSet.add(new Person(x, y));
+            return this.firstPosition - human.firstPosition;
 
         }
 
-        // distance 역순
-        for (Person person : treeSet) {
 
-            Long lowerValue = memoSet.floor(person.x);
-            Long higherValue = memoSet.ceiling(person.y);
+    }
 
-            if (lowerValue == null) {
-                memoSet.add(person.x);
-            }
 
-            if (higherValue == null) {
-                memoSet.add(person.y);
-            }
+    static int n, t;
+    static TreeSet<Human> treeSet = new TreeSet<>();
+
+
+
+    public static void main(String[] args) {
+        setting();
+
+        compareProcess();
+    }
+
+    private static void compareProcess() {
+
+        Human human = treeSet.last();
+        long minPosition = human.lastPosition;
+        int cnt = 0;
+
+        while (treeSet.lower(human) != null) {
+
+            Human frontHuman = treeSet.lower(human);
+
+            if (minPosition <= frontHuman.lastPosition)
+                cnt++;
+            else
+                minPosition = frontHuman.lastPosition;
+
+            human = frontHuman;
+
 
         }
 
-        System.out.print(memoSet.size() - 1);
-
+        System.out.println(n - cnt);
 
 
 
     }
 
+    private static void setting() {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        t = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            treeSet.add(new Human(sc.nextInt(), sc.nextInt()));
+        }
+
+
+    }
 
 
 }
