20d338db9af25cb80e7402e069c380741431b46b
최선빈 <105468810+hurontrara@users.noreply.github.com>
2024-05-13 17:14:01 +0900

    Title: 자리 차지하기, Time: 100ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240513/자리 차지하기/take-place.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n, m; // n명의 사람, m개의 의자
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static TreeSet<Integer> treeSet = new TreeSet<>();
+
+    public static void main(String[] args) throws Exception {
+        
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken()); m = Integer.parseInt(st.nextToken());
+
+        // {1, 2, 3, 4}
+        for (int i = 1; i <= m; i++) {
+            treeSet.add(i);
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+
+            int value = Integer.parseInt(st.nextToken());
+
+            if (treeSet.floor(value) != null) {
+
+                treeSet.remove(treeSet.floor(value));
+                cnt += 1;
+
+            } else {
+
+                System.out.print(cnt);
+                System.exit(0);
+
+            }
+
+        }
+
+
+    }
+
+
+
+}
