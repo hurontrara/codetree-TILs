53cb77dfa676d063a7ec6a93f309903a2d2ddaf7
최선빈 <105468810+hurontrara@users.noreply.github.com>
2024-05-13 20:29:20 +0900

    Title: 최대로 연속한 숫자, Time: 3095ms, Memory: 22MB, Status: Time Limit Exceed - CodeTree

--- 240513/최대로 연속한 숫자/maximum-consecutive-number.java
+++ 240513/최대로 연속한 숫자/maximum-consecutive-number.java
@@ -1,6 +1,31 @@
 import java.io.*;
 import java.util.*;
 
+class State implements Comparable<State> {
+
+    int x;
+    int distance;
+
+    State(int x, int distance) {
+
+        this.x = x;
+        this.distance = distance;
+    }
+
+    @Override
+    public int compareTo(State state) {
+
+        return this.x - state.x;
+
+    }
+
+    State(int x) {
+        this.x = x;
+    }
+
+
+}
+
 
 public class Main {
 
@@ -10,44 +35,38 @@ public class Main {
     static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
 
-    static TreeSet<Integer> treeSet = new TreeSet<>();
+    static TreeSet<State> treeSet = new TreeSet<>();
 
     public static void main(String[] args) throws Exception {
 
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken()); m = Integer.parseInt(st.nextToken());
 
+        treeSet.add(new State(n + 1, n + 1));
         st = new StringTokenizer(br.readLine());
 
         for (int i = 0; i < m; i++) {
 
             int answer = 0;
+            int value = Integer.parseInt(st.nextToken());
 
-            int start = 0;
+            State newState = new State(value);
+            treeSet.add(newState);
+            newState.distance = treeSet.lower(newState) == null ? value : (value - treeSet.lower(newState).x - 1);
 
-            treeSet.add(Integer.parseInt(st.nextToken()));
+            State afterState = treeSet.higher(newState);
+            afterState.distance = afterState.x - value - 1;
 
-            for (int value : treeSet) {
 
-                int end = value;
-
-                answer = Math.max(answer, end - start);
-
-                start = end + 1;
-
+            for (State state : treeSet) {
+                answer = Math.max(answer, state.distance);
             }
 
-            answer = Math.max(answer, n - start + 1);
-
             sb.append(answer).append("\n");
-
 
         }
 
         System.out.print(sb);
 
-
     }
-
-
 }
