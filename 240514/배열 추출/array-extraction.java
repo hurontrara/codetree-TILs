e402ff5d8ecbe68399f5b426874703cb61c7a6a7
최선빈 <105468810+hurontrara@users.noreply.github.com>
2024-05-14 11:29:58 +0900

    Title: 배열 추출, Time: 395ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240514/배열 추출/array-extraction.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+    static int n;
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+
+    static PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+    public static void main(String[] args) throws Exception {
+
+        n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+
+            int order = Integer.parseInt(br.readLine());
+
+            if (order == 0) {
+
+                if (pq.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(pq.poll() * -1).append("\n");
+                }
+
+            } else {
+
+                pq.add(order * - 1);
+
+            }
+
+        }
+
+        System.out.print(sb);
+
+    }
+
+
+
+}
