75691488ad9c238b1106c40e2e0723a117b6e9ec
최선빈 <105468810+hurontrara@users.noreply.github.com>
2024-05-14 21:06:50 +0900

    Title: 배열 추출 2, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240514/배열 추출 2/array-extraction-2.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+
+class Point implements Comparable<Point> {
+
+    int x;
+
+    Point(int x) {
+        this.x = x;
+    }
+
+    @Override
+    public int compareTo(Point point) {
+
+        if (Math.abs(this.x) == Math.abs(point.x)) {
+            return this.x - point.x;
+        }
+
+        return Math.abs(this.x) - Math.abs(point.x);
+
+    } 
+
+}
+
+public class Main {
+
+    static int size;
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+
+    static PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+    public static void main(String[] args) throws Exception {
+
+        size = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < size; i++) {
+
+            int order = Integer.parseInt(br.readLine());
+
+            if (order == 0) {
+
+                if (queue.isEmpty()) {
+
+                    sb.append("0 \n");
+
+                } else {
+
+                    sb.append(queue.poll()).append("\n");
+
+                }
+
+            } else {
+
+                queue.add(order);
+
+            }
+
+
+        }
+
+        System.out.print(sb);
+
+
+    }
+
+
+
+
+}
