1aa0ba02d75b6df8d7b07f2515f3035155b40b09
최선빈 <105468810+hurontrara@users.noreply.github.com>
2024-05-16 22:08:53 +0900

    Title: 배열에서 자리 바꾸기, Time: 3897ms, Memory: 189MB, Status: Passed - CodeTree

--- 240516/배열에서 자리 바꾸기/switch-position-in-array.java
+++ 240516/배열에서 자리 바꾸기/switch-position-in-array.java
@@ -1,139 +1,91 @@
-import java.io.*;
-import java.util.*;
-
-class Node {
-
-    int num;
-    Node prev;
-    Node next;
-
-    Node(int num) {
-        this.num = num;
-    }
-
-}
-
+import java.util.Scanner;
 
 public class Main {
+    public static final int MAX_N = 250005;
 
-    static int size, q;  // size개의 배열, q개의 연산
+    // 한 노드를 나타내는 클래스입니다.
+    static class Node {
+        int id;
+        Node prev, next;
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
-
-    static HashMap<Integer, Node> hashMap = new HashMap<>();
-
-    public static void main(String[] args) throws Exception {
-
-        size = Integer.parseInt(br.readLine());
-        q = Integer.parseInt(br.readLine());
-
-        Node leftNode = new Node(1); hashMap.put(1, leftNode);
-        for (int i = 2; i <= size; i++) {
-
-            Node presentNode = new Node(i);
-
-            presentNode.prev = leftNode; leftNode.next = presentNode;
-
-            hashMap.put(i, presentNode);
-
-            leftNode = presentNode;
-
+        Node(int id) {
+            this.id = id;
+            this.prev = null;
+            this.next = null;
         }
-
-        for (int i = 0; i < q; i++) {
-
-            st = new StringTokenizer(br.readLine());
-
-            int a = Integer.parseInt(st.nextToken()); int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken()); int d = Integer.parseInt(st.nextToken());
-
-            Node aNode = hashMap.get(a); Node bNode = hashMap.get(b); Node cNode = hashMap.get(c); Node dNode = hashMap.get(d);
-
-            if (bNode.next == cNode) {
-
-                bNode.next = dNode.next;
-                if (dNode.next != null)
-                    dNode.prev = bNode;
-                
-                cNode.prev = aNode.prev; dNode.next = aNode;
-
-                if (aNode.prev != null)
-                    aNode.prev.next = cNode;
-                aNode.prev = dNode;
-
-            }
-            else if (dNode.next == aNode) {
-
-                dNode.next = bNode.next;
-                if (dNode.next != null)
-                    dNode.next.prev = bNode;
-
-                aNode.prev = cNode.prev;
-                bNode.next = cNode;
-
-                if (aNode.prev != null)
-                    aNode.prev.next = aNode;
-                cNode.prev = bNode;
-
-            }
-            else {
-
-                Node aSave = aNode.prev; Node bSave = bNode.next;
-
-                aNode.prev = cNode.prev;
-                bNode.next = dNode.next;
-
-                if (aNode.prev != null)
-                    aNode.prev.next = aNode;
-                if (bNode.next != null) {
-                    bNode.next.prev = bNode;
-                }
-
-                cNode.prev = aSave; dNode.next = bSave;
-
-                if (cNode.prev != null)
-                    cNode.prev.next = cNode;
-                if (dNode.next != null)
-                    dNode.next.prev = dNode;
-
-
-            }
-
-
-        }
-
-        Node printNode = hashMap.get(1);
-        while (true) {
-
-            if (printNode.prev == null)
-                break;
-
-            printNode = printNode.prev;
-
-
-        }
-
-        while (true) {
-
-            sb.append(printNode.num).append(" ");
-
-            if (printNode.next == null)
-                break;
-            
-            printNode = printNode.next;
-
-        }
-
-
-        System.out.print(sb);
-
-
-
-
     }
 
+    public static Node[] nodes = new Node[MAX_N];
 
+    // 두 노드를 연결해줍니다.
+    public static void connect(Node s, Node e) {
+        if (s != null)
+            s.next = e;
+        if (e != null)
+            e.prev = s;
+    }
 
+    // 두 노드 사이의 부분 배열을 서로 바꾸는 함수입니다.
+    public static void swapSubarray(Node a, Node b, Node c, Node d) {
+        // 각 노드의 이전 노드와 다음 노드의 연결 상태를 저장합니다.
+        Node after_prevA = c.prev; // a와 연결된 c의 이전 노드
+        Node after_nextB = d.next; // b와 연결된 d의 다음 노드
+
+        Node after_prevC = a.prev; // c와 연결된 a의 이전 노드
+        Node after_nextD = b.next; // d와 연결된 b의 다음 노드
+
+        // b와 c가 바로 인접한 경우, 즉 b 다음에 c가 오는 경우의 예외 처리를 합니다.
+        if (b.next == c) {
+            after_prevA = d;
+            after_nextD = a;
+        }
+        // d와 a가 바로 인접한 경우, 즉 d 다음에 a가 오는 경우의 예외 처리를 합니다.
+        if (d.next == a) {
+            after_nextB = c;
+            after_prevC = b;
+        }
+
+        // 노드들을 새로운 연결 상태에 맞게 연결해줍니다.
+        connect(after_prevA, a);
+        connect(b, after_nextB);
+
+        connect(after_prevC, c);
+        connect(d, after_nextD);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // N개의 노드를 생성합니다.
+        for (int i = 1; i <= n; i++)
+            nodes[i] = new Node(i);
+
+        // 1부터 N번 까지의 노드를 차례로 연결해줍니다.
+        for (int i = 1; i < n; i++)
+            connect(nodes[i], nodes[i + 1]);
+
+        int q = sc.nextInt();
+
+        // 연산을 진행합니다.
+        for (int i = 0; i < q; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            int d = sc.nextInt();
+
+            swapSubarray(nodes[a], nodes[b], nodes[c], nodes[d]);
+        }
+
+        // 연산이 끝나고 제일 앞에 있는 노드를 찾습니다.
+        Node cur = nodes[1];
+        while (cur.prev != null)
+            cur = cur.prev;
+
+        // 해당 노드부터 끝까지 출력을 합니다.
+        while (cur != null) {
+            System.out.print(cur.id + " ");
+            cur = cur.next;
+        }
+    }
 }
