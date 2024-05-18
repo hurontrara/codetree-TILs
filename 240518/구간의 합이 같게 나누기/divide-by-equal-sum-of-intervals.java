51a5b87bd99e2f69b265e5133a623d363ff4b397
최선빈 <105468810+hurontrara@users.noreply.github.com>
2024-05-18 18:01:16 +0900

    Title: 구간의 합이 같게 나누기, Time: 255ms, Memory: 22MB, Status: Wrong Answer - CodeTree

---
+++ 240518/구간의 합이 같게 나누기/divide-by-equal-sum-of-intervals.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+    static int n;
+
+    static int total = 0;
+
+    static int[] array;
+    static int[] sumArray;
+
+    static int[] firstCountArray;
+    static int[] secondCountArray;
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+
+        n = Integer.parseInt(br.readLine());
+        array = new int[n]; sumArray = new int[n];
+        firstCountArray = new int[n]; secondCountArray = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+
+        // array + total
+        for (int i = 0; i < n; i++) {
+            int input = Integer.parseInt(st.nextToken());
+            array[i] = input;
+            total += input;
+        }
+
+        // 누적합
+        sumArray[0] = array[0];
+
+        for (int i = 1; i < n; i++) {
+            sumArray[i] = sumArray[i - 1] + array[i];
+        }
+
+        // 3의 갯수 / 9의 갯수
+        if (array[0] == total / 4)
+            firstCountArray[0] = 1;
+        if (array[0] == total / 4 * 3)
+            secondCountArray[0] = 1;
+
+
+        for (int i = 1; i < n; i++) {
+
+            firstCountArray[i] = firstCountArray[i - 1];
+            secondCountArray[i] = secondCountArray[i - 1];
+
+            if (sumArray[i] == total / 4)
+                firstCountArray[i] += 1;
+            if (sumArray[i] == total / 4 * 3)
+                secondCountArray[i] += 1;
+
+        }
+
+        int answer = 0;
+        for (int i = 1; i < n - 2; i++) {
+
+            if (sumArray[i] == total / 2) {
+
+                answer += firstCountArray[i - 1] * (secondCountArray[n - 2] - secondCountArray[i]);
+
+            }
+
+        }
+        System.out.print(answer);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
