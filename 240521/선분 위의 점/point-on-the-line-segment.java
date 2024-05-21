5f1c068dfad8e00314db8f66dcbe8c32a9289557
최선빈 <105468810+hurontrara@users.noreply.github.com>
2024-05-21 22:05:28 +0900

    Title: 선분 위의 점, Time: 185ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 240521/선분 위의 점/point-on-the-line-segment.java
@@ -0,0 +1,98 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+    static int n, m; // n개의 점 , 4개의 선분(쿼리)
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    static int[] array;
+
+    public static void main(String[] args) throws Exception {
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken()); m = Integer.parseInt(st.nextToken());
+        array = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < m; i++) {
+
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken()); int b = Integer.parseInt(st.nextToken());
+
+            // 작은 것 중에 가장 큰 것
+            // 작거나 같은 것 중에 가장 큰 것
+
+            int first = getLowerBound(a);
+            int second = getUpperBound(b);
+
+            sb.append(second - first).append("\n");
+
+        }
+        System.out.print(sb);
+
+
+    }
+
+    static int getLowerBound(int input) {
+
+        int left = 0; int right = n - 1;
+
+        int idx = -1;
+        while (left <= right) {
+
+            int mid = (left + right) / 2;
+
+            if (array[mid] < input) {
+
+                left = mid + 1;
+                idx = Math.max(idx, mid);
+
+            }
+            else {
+                right = mid - 1;
+            }
+
+
+        }
+        return idx;
+
+    }
+
+    static int getUpperBound(int input) {
+
+        int left = 0; int right = n - 1;
+
+        int idx = -1;
+        while (left <= right) {
+
+            int mid = (left + right) / 2;
+
+            if (array[mid] <= input) {
+
+                left = mid + 1;
+                idx = Math.max(idx, mid);
+
+            }
+            else {
+                right = mid - 1;
+            }
+
+
+        }
+        return idx;
+
+
+    }
+
+
+
+}
