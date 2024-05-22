28fdb23553c8866b886151537cb1386b44cc3a30
최선빈 <105468810+hurontrara@users.noreply.github.com>
2024-05-22 12:15:21 +0900

    Title: 이차원 배열의 오름차순 정리, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240522/이차원 배열의 오름차순 정리/ascending-order-of-two-dimensional-array.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n, k; // n * n 의 행렬, k번째 오는 수
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    public static void main(String[] args) throws Exception {
+
+        n = Integer.parseInt(br.readLine()); k = Integer.parseInt(br.readLine());
+
+        if (k == 1) {
+            System.out.print(1);
+            System.exit(0);
+        }
+
+        long left = 1; long right = (long) Math.min(Math.pow(10, 9), Math.pow(n, 2));
+        long idx = 0;
+        while (left <= right) {
+
+            long mid = (left + right) / 2;
+
+            // mid가 몇 번째인지
+            long count = 0;
+            for (int i = 1; i <= mid; i++) {
+                count += (mid / i);
+            }
+
+            if (count <= k) {
+                idx = Math.max(idx, mid);
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+
+
+        }
+        System.out.print(idx);
+
+
+    }
+
+
+
+
+
+}
