f1424ffd3343e0cbbdd81f744da412fa6dfb34c6
최선빈 <105468810+hurontrara@users.noreply.github.com>
2024-05-28 19:02:09 +0900

    Title: 계단 오르기 2, Time: 85ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240528/계단 오르기 2/climbing-stairs-2.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n;
+    static int[] array;
+
+    static int[][] dpMatrix;
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+
+        n = Integer.parseInt(br.readLine());
+        array = new int[n + 1];
+        dpMatrix = new int[n + 1][4];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 초기화
+        dpMatrix[1][1] = array[1];
+        dpMatrix[2][0] = array[2];
+
+        // logic
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 4; j++) {
+                
+                if (dpMatrix[i][j] > 0) {
+
+                    int firstRow = i + 1; int firstCol = j + 1;
+                    int secondRow = i + 2; int secondCol = j;
+
+                    if (existCheck(firstRow, firstCol)) {
+                        dpMatrix[firstRow][firstCol] = dpMatrix[i][j] + array[firstRow];
+                    }
+
+                    if (existCheck(secondRow, secondCol)) {
+                        dpMatrix[secondRow][secondCol] = dpMatrix[i][j] + array[secondRow];
+                    }
+
+                }
+
+            }
+        }
+
+        // print
+        int answer = 0;
+        for (int i = 0; i < 4; i++) {
+            answer = Math.max(answer, dpMatrix[n][i]);
+        }
+        System.out.print(answer);
+
+
+    }
+
+    static boolean existCheck(int row, int col) {
+        return row >= 0 && row <= n && col >= 0 && col < 4;
+    }
+}
