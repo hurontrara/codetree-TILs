1f396888250bc0c709b7b6c5c30653271fd9c6fc
최선빈 <105468810+hurontrara@users.noreply.github.com>
2024-05-29 15:21:20 +0900

    Title: 게임 스튜디오에서 살아남기, Time: 81ms, Memory: 8MB, Status: Passed - CodeTree

--- 240529/게임 스튜디오에서 살아남기/surviving-in-a-game-studio.java
+++ 240529/게임 스튜디오에서 살아남기/surviving-in-a-game-studio.java
@@ -1,44 +1,61 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+
 
 public class Main {
-    public static final int MOD = 1000000007;
-    public static final int MAXN = 1005;
-    public static int n;
-    public static int[][][] dp = new int[MAXN][5][5];
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    static int n;
 
-        // n 값을 입력받습니다.
-        n = sc.nextInt();
+    static int[][][] dpMatrix;
 
-        // 초기 상태를 설정합니다. 
-        dp[1][1][0] = 1;  // 첫 번째 날에 T를 받은 경우
-        dp[1][0][1] = 1;  // 첫 번째 날에 B를 받은 경우
-        dp[1][0][0] = 1;  // 첫 번째 날에 G를 받은 경우
+    static int MOD = (int) 1e9 + 7;
 
-        // 동적 프로그래밍을 사용해 문제를 해결합니다.
-        // dp[i][j][k] :: i번째 날에, T를 총합 j회 받았고, B를 최근 k회 연속 받은 경우의 가짓수
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+
+        n = Integer.parseInt(br.readLine());
+        dpMatrix = new int[n + 1][3 + 1][3 + 1]; // 0, 1, 2 안에서만 돎 (두번째가 T, 세번째가 B)
+
+        
+        // init
+        dpMatrix[1][0][0] = 1; dpMatrix[1][1][0] = 1; dpMatrix[1][0][1] = 1;
+
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < 3; j++) {
                 for (int k = 0; k < 3; k++) {
-                    // 다음 날로 넘어가는 경우의 수를 갱신합니다.
-                    dp[i + 1][j + 1][0] = (dp[i + 1][j + 1][0] + dp[i][j][k]) % MOD;
-                    dp[i + 1][j][0] = (dp[i + 1][j][0] + dp[i][j][k]) % MOD;
-                    dp[i + 1][j][k + 1] = (dp[i + 1][j][k + 1] + dp[i][j][k]) % MOD;
+
+                    if (dpMatrix[i][j][k] != 0) {
+
+                        dpMatrix[i + 1][j][0] += dpMatrix[i][j][k];
+                        dpMatrix[i + 1][j + 1][0] += dpMatrix[i][j][k];
+                        dpMatrix[i + 1][j][k + 1] += dpMatrix[i][j][k];
+
+
+                        dpMatrix[i + 1][j][0] %= MOD;
+                        dpMatrix[i + 1][j + 1][0] %= MOD;
+                        dpMatrix[i + 1][j][k + 1] %= MOD;
+
+                    }
+
+
+
                 }
             }
         }
 
-        // 최종 결과를 계산합니다.
-        int ans = 0;
+        int answer = 0;
         for (int j = 0; j < 3; j++) {
             for (int k = 0; k < 3; k++) {
-                ans = (ans + dp[n][j][k]) % MOD;
+                answer += dpMatrix[n][j][k];
+                answer %= MOD;
             }
         }
+        System.out.print(answer);
 
-        // 결과를 출력합니다.
-        System.out.println(ans);
+
+
     }
+
+
 }
