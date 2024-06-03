26b671d39e31f4bd25c4174415ff87ec47ccbf05
최선빈 <105468810+hurontrara@users.noreply.github.com>
2024-06-03 17:22:42 +0900

    Title: 가장 많이 겹치는 구간, Time: 504ms, Memory: 22MB, Status: Passed - CodeTree

---
+++ 240603/가장 많이 겹치는 구간/section-with-maximum-overlap.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+    static int n;
+    static int[] array;
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+
+        n = Integer.parseInt(br.readLine());
+        array = new int[200000 + 1];
+
+        for (int i = 0; i < n; i++) {
+
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken()); int b = Integer.parseInt(st.nextToken());
+
+            array[a] = 1; array[b] = -1;
+
+
+        }
+
+        int answer = 0;
+        int sum = 0;
+        for (int i = 0; i < 200000; i++) {
+
+            sum += array[i];
+            answer = Math.max(answer, sum);
+
+        }
+        System.out.print(answer);
+
+
+    }
+
+
+}
